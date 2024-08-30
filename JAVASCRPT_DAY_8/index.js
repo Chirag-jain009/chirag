f9170c9a4e0580a0417ab6d50cefcd95b0b7d5b2
chirag <chiragjain009@gmail.com>
2024-08-30 15:40:15 +0530

    JS

---
+++ JAVASCRPT_DAY_8/index.js
@@ -0,0 +1,9 @@
+/* console.log("hello world","Chirag", "Jain");
+console.log("hello world","Chirag", "Jain");
+console.log("hello world","Chirag", "Jain");
+console.log("hello world","Chirag", "Jain");
+console.log("hello world","Chirag", "Jain");
+console.log("hello world","Chirag", "Jain"); */
+let name = "Chirag Jain";
+var name1 = "Chirag Jain";
+const name2 = "Chirag jain";
